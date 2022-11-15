35e232608964b8593805a7cbf3154f6c3b77418e
mdpjulian <mdpjulian@gmail.com>
2022-11-15 20:36:24 +0000

    removed dump from main.cpp

--- ticket-to-ride/main.cpp
+++ ticket-to-ride/main.cpp
@@ -11,36 +11,12 @@
 
 int main(){
 
-    //ClientController clientCl ;
-    //ManagerController managerCl;
-    //TicketController ticketCl;
-
-
-
     Menu* ticketMenu = new Menu();
     ticketMenu->display();
 
+
+
     delete ticketMenu;
-/*
-    clientCl->displayClients();
-    managerCl.displayManagers();
-
-
-    //ticket con id erroneo, falla.
-    ticketCl.createTicket(2938473, "Consulta sobre una compra", clientCl.getClientObject(31), managerCl.getManagerObject(102));
-
-    ticketCl.createTicket(1315524, "Problema en el envio", clientCl.getClientObject(123), managerCl.getManagerObject(164));
-
-
-    ticketCl.updateTicket(2938473, "nueva modificacion msg", "PENDIENTE");
-    ticketCl.displayTickets();
-
-*/
-    
-
-
-
-
     return 0;
 
 }
